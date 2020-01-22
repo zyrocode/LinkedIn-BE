ea8d9437e960505baf84e9678949e76e6eb49e88
zyrocode <56383676+zyrocode@users.noreply.github.com>
2020-01-22 16:12:00 +0100

    pdf generator added

--- server.js
+++ server.js
@@ -1,19 +1,35 @@
-const express = require ('express');
+const express = require('express');
 const app = express();
-const dotenv = require ('dotenv');
+const dotenv = require('dotenv');
+const cors = require("cors");
 const mongoose = require('mongoose');
 const userServices = require('./src/services/users/index');
 dotenv.config();
 
-mongoose.connect("mongodb://127.0.0.1:27017/linkedindb",{useNewUrlParser:true, useUnifiedTopology:true, useFindAndModify:true})
-    .then(db=>{
-        console.log('mongoose db is live')
-    },
-     err => console.log('mongoose db failed to connect', err))
+const whitelist = ["http://localhost:3000"];
+var corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    }
+};
+
+mongoose.connect("mongodb://127.0.0.1:27017/linkedindb", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: true
+    })
+    .then(db => {
+            console.log('mongoose db is live')
+        },
+        err => console.log('mongoose db failed to connect', err))
 
 
 app.use(express.json());
-app.use('/users', userServices);
-app.listen(process.env.PORT ,()=>{
+app.use('/users', cors(corsOptions),userServices);
+app.listen(process.env.PORT, () => {
     console.log(`server active on port ${process.env.PORT}`);
 })
--- src/services/users/index.js
+++ src/services/users/index.js
@@ -1,49 +1,54 @@
 const express = require('express');
 const router = express.Router();
-
-const user = require ('../models/users/index');
-
+const user = require('../models/users/index');
+const pdfGenerator = require("../../pdf/pdfgenerator")
+const path = require("path")
+const fs = require("fs-extra")
 
 //USER
 //GET all users
-router.get('/', async (req,res)=>{
+router.get('/', async (req, res) => {
     const results = await user.find({})
     console.log('Fetching data');
     res.send(results);
 })
 
 //GET user by id
-router.get('/:id', async (req,res)=>{
+router.get('/:id', async (req, res) => {
     const result = await user.findById(req.params.id);
     console.log('Fetching data by Id');
     res.send(result)
 })
 
 //POST user
-router.post('/', async(req,res)=>{
-    try{
+router.post('/', async (req, res) => {
+    try {
         const result = await user.create(req.body);
         console.log('Posting data');
         res.send(result)
-    } catch(err){
+    } catch (err) {
         res.send(err)
         console.log(err)
     }
 })
-router.put('/:id', async(req,res)=>{
-    try{
-        const toEdit = user.findByIdAndUpdate(req.params.id, {$set: {...req.body}});
+router.put('/:id', async (req, res) => {
+    try {
+        const toEdit = user.findByIdAndUpdate(req.params.id, {
+            $set: {
+                ...req.body
+            }
+        });
         res.send(toEdit);
         console.log('editing data');
-    } catch(err){
+    } catch (err) {
         console.log(err)
     }
 })
 
-router.delete('/:id', async(req,res)=>{
-    try{
+router.delete('/:id', async (req, res) => {
+    try {
         const deleted = user.findByIdAndDelete(req.params.id);
-    } catch (err){
+    } catch (err) {
         console.log(err)
         res.status(500).send('server error')
     }
@@ -52,24 +57,34 @@ router.delete('/:id', async(req,res)=>{
 })
 
 //Experience
-router.get('/:id/experience', async(req,res)=>{
+router.get('/:id/experience', async (req, res) => {
     console.log('Fetching all experience from a user');
 })
 
-router.post('/:id/experience/:expid', async (req,res)=>{
+router.post('/:id/experience/:expid', async (req, res) => {
     console.log('fetching 1 experience from a user')
 })
 
-router.post('/:id/experience', async(req,res)=>{
+router.post('/:id/experience', async (req, res) => {
     console.log('posting experience to a user');
 })
 
-router.put('/:id/experience/:expid', async(req,res)=>{
+router.put('/:id/experience/:expid', async (req, res) => {
     console.log('editing an experience from a user')
 })
 
-router.delete('/:id/experience/:expid', async (req,res)=>{
+router.delete('/:id/experience/:expid', async (req, res) => {
     console.log('deleting an experience from a user');
 })
 
-module.exports = router; 
+//PDF
+router.get('/:id/pdf', async (req, res) => {
+    console.log('pdf');
+    const response = await user.findById(req.params.id)
+    await pdfGenerator(response)
+    const file = path.join(__dirname, `../../pdf/${response._id}.pdf`);
+    res.setHeader("Content-Disposition", `attachment; filename=${response._id}.pdf`);
+    fs.createReadStream(file).pipe(res);
+})
+
+module.exports = router;
