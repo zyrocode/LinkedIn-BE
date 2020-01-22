a51e38bc1720ba1736ff2e94592ff8832cf73220
infinityshades <tash.aneiq@gmail.com>
2020-01-22 13:25:35 +0100

    CRUD FOR EXPERIENCE DONE

--- src/services/users/index.js
+++ src/services/users/index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const user = require ('../models/users/index');
-
+const { ObjectId } = require ('mongodb');
 
 //USER
 //GET all users
 router.get('/', async (req,res)=>{
-    const results = await user.find({})
+    const results = await user.find({});
     console.log('Fetching data');
     res.send(results);
 })
@@ -16,7 +16,7 @@ router.get('/', async (req,res)=>{
 router.get('/:id', async (req,res)=>{
     const result = await user.findById(req.params.id);
     console.log('Fetching data by Id');
-    res.send(result)
+    res.send(result);
 })
 
 //POST user
@@ -24,10 +24,10 @@ router.post('/', async(req,res)=>{
     try{
         const result = await user.create(req.body);
         console.log('Posting data');
-        res.send(result)
+        res.send(result);
     } catch(err){
         res.send(err)
-        console.log(err)
+        console.log(err);
     }
 })
 
@@ -39,7 +39,7 @@ router.post('/', async(req,res)=>{
 router.put('/:id', async(req,res)=>{
     try{
         console.log('editing data');
-        delete req.body._id
+        delete req.body._id;
         const edited = await user.findByIdAndUpdate(req.params.id, {$set: {...req.body}});
         if (edited) {
             res.send(edited);
@@ -47,7 +47,7 @@ router.put('/:id', async(req,res)=>{
             res.status(404).send('user not found');
         }
     } catch(err){
-        console.log(err)
+        console.log(err);
     }
 })
 
@@ -60,33 +60,78 @@ router.delete('/:id', async(req,res)=>{
             res.status(404).send('user not found');
         }
     } catch (err){
-        console.log(err)
-        res.status(500).send('server error')
+        console.log(err);
+        res.status(500).send('server error');
     }
 
     console.log('deleting data');
 })
 
 //Experience
+//GET All experience from user
 router.get('/:id/experience', async(req,res)=>{
-    const experience = await user.findById(req.body)
+    const result = await user.findById(req.params.id);
+    res.send(result.experience)
     console.log('Fetching all experience from a user');
 })
 
-router.post('/:id/experience/:expid', async (req,res)=>{
+//GET specific experience from user
+router.get('/:id/experience/:expid', async (req,res)=>{
+    const singleExperience = await user.findOne(
+        {
+        _id: new ObjectId(req.params.id),
+        "experience._id": new ObjectId(req.params.expid)
+    },{
+            "experience.$":1
+    })
+    // const singleExperience = await user.findById(req.params.id, experience._id = req.params.expid)
+    res.send(singleExperience);
     console.log('fetching 1 experience from a user')
 })
 
+//POST experience on user
 router.post('/:id/experience', async(req,res)=>{
     console.log('posting experience to a user');
+    const newExperience = req.body
+    try{
+        const experience = await user.findByIdAndUpdate(req.params.id, {$push:{experience: newExperience}})
+        res.send(experience);
+    } catch (err) {
+        res.status(500).send('server error');
+    }
 })
 
-router.put('/:id/experience/:expid', async(req,res)=>{
+//EDIT specific experience on a specific user
+router.patch('/:id/experience/:expid', async(req,res)=>{
+    try {
+        const experience = await user.updateOne(
+        {
+            _id: new ObjectId(req.params.id),
+            "experience._id":new ObjectId(req.params.expid)
+        },{
+            "experience.$":req.body
+        })
+        res.send('edited');
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
     console.log('editing an experience from a user')
 })
 
+
+//DELETE a specific experience from a specific user
 router.delete('/:id/experience/:expid', async (req,res)=>{
-    console.log('deleting an experience from a user');
+    try{
+        console.log('deleting an experience from a user');
+        const toDelete = await user.findByIdAndUpdate(req.params.id, 
+            { $pull: {experience: {_id: new ObjectId(req.params.expid)}}    
+        });
+        res.send('Removed experience');
+    } catch (err){
+        console.log(err);
+        res.status(500).send(err);
+    }
 })
 
 module.exports = router; 
